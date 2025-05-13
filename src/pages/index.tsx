0dec94c6bbc7045b1bf8bc85970e65d9ed462315
ramong26 <rlatndus21049@gmail.com>
2025-05-13 20:59:48 +0900

    랜딩페이지타이ㅊ가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,28 @@ const poppins = Poppins({
 export default function Home() {
   return (
     <>
-      <header className="w-full">헤더</header>
-      <div
-        className={`${styles.bgAnimate} h-screen flex justify-center items-center flex-col`}
-      >
-        <section>
-          <h1
+      <div className={`${styles.bgAnimate} h-screen `}>
+        <nav className="w-full bg-[#1000004d] backdrop-blur-lg text-amber-50 flex items-center justify-between p-7 fixed">
+          <div>헤더 로고</div>
+          <div className="flex gap-6">
+            <div>로그인</div>
+            <div>회원가입</div>
+          </div>
+        </nav>
+        <section className="flex items-center justify-center pt-40">
+          <header
             className={`${poppins.className} ${styles.titleAnimate} font-bold text-9xl`}
           >
             Global Top 50
-          </h1>
+          </header>
         </section>
-        <section>최신곡 정보</section>
+        <div className="flex items-center justify-between flex-col h-full">
+          <section>현재 인기 차트 탑 5 카드로 애니메이션 차트 보러가기</section>
+          <section>최신곡 1개 차트 보러가기</section>
+          <section>추천음악</section>
+          <section>추천 플레이리스트</section>
+          <section>footer</section>
+        </div>
       </div>
     </>
   )
