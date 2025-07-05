e9a4534ca3994e1e39cb9065d4731fa1263cad48
ramong26 <rlatndus21049@gmail.com>
2025-07-05 13:22:54 +0900

    로그아웃수정

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const response = NextResponse.json({ success: true });
+  response.cookies.set({
+    name: "access_token",
+    value: "",
+    maxAge: 0,
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  });
+  return response;
+}
