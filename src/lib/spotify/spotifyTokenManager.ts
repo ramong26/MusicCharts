e5efe99dc7c6cf1bfe500c8352591f4d0f57efbc
ramong26 <rlatndus21049@gmail.com>
2025-07-23 22:38:04 +0900

    배포수정4

--- src/lib/spotify/spotifyTokenManager.ts
+++ src/lib/spotify/spotifyTokenManager.ts
@@ -2,7 +2,6 @@ let cachedToken: string | null = null;
 
 export async function getSpotifyAccessToken() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://127.0.0.1:3000';
-  const now = Date.now();
 
   const tokenRes = await fetch(`${baseUrl}/api/spotify-token`);
   const data = await tokenRes.json();
