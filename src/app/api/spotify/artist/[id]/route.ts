a75c0f8998dadd0116501cf26f4bb01b8fc6cf44
김수연 <rlatndus21049@gmail.com>
2025-07-26 18:15:04 +0900

    2차 배포 2

--- src/app/api/spotify/artist/[id]/route.ts
+++ src/app/api/spotify/artist/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import getArtist from '@/features/tracks/hooks/getArtist';
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET({ params }: { params: { id: string } }) {
   try {
     const artist = await getArtist(params.id);
     return NextResponse.json(artist);
