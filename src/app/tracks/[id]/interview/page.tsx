68434398ccaefc3ae357f7675f85eebbfbce0ccf
김수연 <rlatndus21049@gmail.com>
2025-07-26 18:33:15 +0900

    2차배포 7

--- src/app/tracks/[id]/interview/page.tsx
+++ src/app/tracks/[id]/interview/page.tsx
@@ -1,3 +1,5 @@
+/// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 import getTrackId from '@/features/tracks/hooks/getTrackId';
 import getArtist from '@/features/tracks/hooks/getArtist';
 
