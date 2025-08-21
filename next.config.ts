70d1c4e9bf1ee8af3d1741e9805dbb2544d8a77f
김수연 <rlatndus21049@gmail.com>
2025-08-21 12:36:40 +0900

    임시저장

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const withAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   images: {
@@ -60,5 +65,4 @@ const nextConfig: NextConfig = {
     return config;
   },
 };
-
-export default nextConfig;
+export default withAnalyzer(nextConfig);
